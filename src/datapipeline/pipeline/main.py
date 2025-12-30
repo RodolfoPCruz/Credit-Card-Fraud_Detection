fa5efdb73ef066c32a8a6b537265190c732ed181
RodolfoPCruz <rodolfo_pinheiro@hotmail.com>
2025-12-30 13:01:36 -0300

    feat: add main pipeline orchestrator
    Adds a main script responsible for orchestrating pipeline steps,
    controlling their execution order, and enabling execution from a
    specific pipeline stage.

---
+++ src/datapipeline/pipeline/main.py
@@ -0,0 +1,90 @@
+import argparse
+import yaml
+import mlflow
+import logging
+
+from enum import Enum
+from datapipeline.data.load_data import load_raw_data
+from datapipeline.data.clean_data import clean_data
+from datapipeline.config.logging_config import setup_logging
+
+class Stage(str, Enum):
+    INGEST = 'ingest'
+    CLEAN = 'clean'
+
+PIPELINE_STAGES = [
+    Stage.INGEST,
+    Stage.CLEAN,
+]
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Credit Card Fraud Detection Pipeline')
+    parser.add_argument(
+        '--config',
+        type=str,
+        help='Path to the configuration file',
+        required=True
+    )
+    parser.add_argument(
+        '--stage',
+        type=Stage,
+        help='Stage of the pipeline',
+        required=True,
+        default=Stage.INGEST, 
+        choices=list(Stage)
+    )
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+    with open(args.config, 'r') as f:
+        config = yaml.safe_load(f)
+
+    # Logging
+    log_path = config['pipeline']['log_path']
+    setup_logging(log_path)
+    logger = logging.getLogger('pipeline')
+
+    #MLFlow
+    mlflow.set_experiment(config['pipeline']['experiment_name'])
+
+    with mlflow.start_run(run_name='pipeline_execution'):
+
+        start_idx = PIPELINE_STAGES.index(args.stage)
+        logger.info(f'Starting pipeline from stage: {args.stage}')
+
+        #---------------Data Ingestion----------------------------
+        if Stage.INGEST in PIPELINE_STAGES[start_idx:]:
+            with mlflow.start_run(run_name='data_ingestion', nested=True):
+                schema_path = config['data_ingestion']["schema_path"]
+                with open(schema_path, "r") as f:
+                    schema = yaml.safe_load(f)
+                df, dataset_hash = load_raw_data(
+                    dataset_path=config['data_ingestion']['dataset_path'],
+                    schema=schema
+                )
+
+                mlflow.set_tag("schema_version", schema["schema_version"])
+                mlflow.set_tag("dataset_name", schema["dataset_name"])
+
+                mlflow.log_param("dataset_hash", dataset_hash)
+                mlflow.log_param("dataset_path", config['data_ingestion']['dataset_path'])
+
+                mlflow.log_metric("n_rows", df.shape[0])
+                mlflow.log_metric("n_columns", df.shape[1])
+
+                mlflow.log_artifact(schema_path, artifact_path="schema")
+
+        #---------------Data Cleaning----------------------------
+        if Stage.CLEAN in PIPELINE_STAGES[start_idx:]:
+            with mlflow.start_run(run_name='data_cleaning', nested=True):
+                df, removed_rows = clean_data(
+                    df=df,
+                    target_column=config['data_clean']['target_column']
+                )
+
+                mlflow.log_metric("rows_removed", removed_rows)
+
+if __name__ == "__main__":
+    main()
